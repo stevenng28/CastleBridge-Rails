c0cc4140025a2b460c4d5f4a1fda18c5ee3af707
Steven Ng <stevenng132@gmail.com>
2017-01-03 01:12:23 +0000

    fixing gemfile for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "notification_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }  
+  config.action_mailer.default_url_options = { :host => 'https://protected-chamber-47366.herokuapp.com/' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {  
   address: "smtp.gmail.com",
   port: 587,
-  domain: "myapp.herokuapp.com",
+  domain: "https://protected-chamber-47366.herokuapp.com/",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
