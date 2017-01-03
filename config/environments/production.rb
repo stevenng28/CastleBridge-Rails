9d8acdab58f67921abe91ca824bc19e07a611951
Steven Ng <stevenng132@gmail.com>
2017-01-03 03:22:27 +0000

    Adjusting smtp settings again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
     port: 587,
-    domain: "heroku.com",
+    domain: "https://protected-chamber-47366.herokuapp.com/",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
