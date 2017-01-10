348a06a14ac2e7f2275da2169f0c490f80c8d7ef
Steven Ng <stevenng132@gmail.com>
2017-01-10 09:04:32 +0000

    Adding update action to BridgeStatus

--- app/controllers/bridge_status_controller.rb
+++ app/controllers/bridge_status_controller.rb
@@ -1,9 +1,27 @@
 class BridgeStatusController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def edit
     render html: 'editing page'
   end
   
   def update
-    render html: 'updating page'
+    @bridge_status = BridgeStatus.first
+    update_symbol = status_params.key('true')
+    if update_symbol
+      if @bridge_status.update_attribute(update_symbol, Time.now)
+        render html: 'Update success'
+      else
+        render html: 'Update failed'
+      end
+    else
+      render html: 'No update made'
+    end
   end
+  
+  private
+  
+    def status_params
+      params.require(:status).permit(:closed, :open, :closing, :opening)
+    end
 end
